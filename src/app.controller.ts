9ea0ce69bd8cf727431a0d54a7cb5157fb935a31
Andreas Andersen <aa@blue-ocean-robotics.com>
2023-08-30 19:56:15 +0200

    feat: add parseEPG method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
-@Controller()
+@Controller('epg')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('parse')
+  parseProgram(@Req() request: Request): string {
+    return this.appService.parseEPG(request);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  parseEPG(request: Request): string {
+    console.log(request.body);
+    return 'Monday: Nyhederne 6 – 10 / Dybvaaaaad 10 – 10:35';
   }
 }
