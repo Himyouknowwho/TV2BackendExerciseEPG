3dd3acd759c9e82929baddd9412a2eebf58649a2
Andreas Andersen <aa@blue-ocean-robotics.com>
2023-09-01 14:56:18 +0200

    feat: :sparkles: add getTimeFromSeconds parser

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Request } from 'express';
-
+import { toDate } from 'date-fns';
 @Injectable()
 export class AppService {
   getHello(): string {
@@ -9,5 +9,14 @@ export class AppService {
   parseEPG(request: Request): string {
     console.log(request.body);
     return 'Monday: Nyhederne 6 – 10 / Dybvaaaaad 10 – 10:35';
+  }
+
+  getTimeFromSeconds(seconds: number): string {
+    const date = toDate(seconds * 1000);
+    const timeString = date.toISOString().split('T')[1].split(':');
+
+    return `${timeString[0]}${
+      timeString[1] !== '00' ? ':' + timeString[1] : ''
+    }`;
   }
 }
