305cb679e8763767c7a67a5e8f39a2fa2f0153f8
Andreas Andersen <aa@blue-ocean-robotics.com>
2023-09-02 12:49:23 +0200

    test(Testing): :white_check_mark: reuse app.controller test in e2e test
    the component test was designed more like a e2e test, so better have it in the right places

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,184 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('should return a days program in a human readable form', async () => {
+    // const req =
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/epg/parse')
+      .send(mondayJSON)
+      .expect(201)
+      .then(async (response) => {
+        expect(response.text.replace(/\s/g, '')).toEqual(
+          humanReadableMonday.replace(/\s/g, ''), //replace whitespaces to only compare content
+        );
+      });
   });
+  it('should return a full weeks program in a human readable form', async () => {
+    return request(app.getHttpServer())
+      .post('/epg/parse')
+      .send(fullProgramJson)
+      .expect(201)
+      .then(async (response) => {
+        expect(response.text.replace(/\s/g, '')).toEqual(
+          fullHumanReadableProgram.replace(/\s/g, ''), //replace whitespaces to only compare content
+        );
+      });
+  });
+
+  const mondayJSON = {
+    monday: [
+      {
+        title: 'Nyhederne',
+        state: 'begin',
+        time: 21600,
+      },
+      {
+        title: 'Nyhederne',
+        state: 'end',
+        time: 36000,
+      },
+      {
+        title: 'Dybvaaaaad',
+        state: 'begin',
+        time: 36000,
+      },
+      {
+        title: 'Dybvaaaaad',
+        state: 'begin',
+        time: 38100,
+      },
+    ],
+  };
+
+  const humanReadableMonday = `Monday: Nyhederne 6 - 10 / Dybvaaaaad 10 - 10:35\n
+Tuesday: Nothing aired today\n
+Wednesday: Nothing aired today\n
+Thursday: Nothing aired today\n
+Friday: Nothing aired today\n
+Saturday: Nothing aired today\n
+Sunday: Nothing aired today\n`;
+
+  const fullHumanReadableProgram = `Monday: Nyhederne 6 - 10 / Dybvaaaaad 10 - 10:35
+Tuesday: Nothing aired today
+Wednesday: Nyhederne 6 - 12, 21 - 21:30 / Fodbold 14 - 15:30
+Thursday: ESL 12 - 13 / ESLPro 23 - 1
+Friday: Nothing aired today
+Saturday: Comedy 14:30 - 16:30 / Nybyggerne 22:40 - 1:30
+Sunday: Dybvaaaaad 11:30 - 12`;
+
+  const fullProgramJson = {
+    monday: [
+      {
+        title: 'Nyhederne',
+        state: 'begin',
+        time: 21600,
+      },
+      {
+        title: 'Nyhederne',
+        state: 'end',
+        time: 36000,
+      },
+      {
+        title: 'Dybvaaaaad',
+        state: 'begin',
+        time: 36000,
+      },
+      {
+        title: 'Dybvaaaaad',
+        state: 'begin',
+        time: 38100,
+      },
+    ],
+    tuesday: [],
+    wednesday: [
+      {
+        title: 'Nyhederne',
+        state: 'begin',
+        time: 21600,
+      },
+      {
+        title: 'Nyhederne',
+        state: 'end',
+        time: 43200,
+      },
+      {
+        title: 'Fodbold',
+        state: 'begin',
+        time: 50400,
+      },
+      {
+        title: 'Fodbold',
+        state: 'end',
+        time: 55800,
+      },
+      {
+        title: 'Nyhederne',
+        state: 'begin',
+        time: 75600,
+      },
+      {
+        title: 'Nyhederne',
+        state: 'end',
+        time: 77400,
+      },
+    ],
+    thursday: [
+      {
+        title: 'ESL',
+        state: 'begin',
+        time: 43200,
+      },
+      {
+        title: 'ESL',
+        state: 'end',
+        time: 46800,
+      },
+      {
+        title: 'ESLPro',
+        state: 'begin',
+        time: 82800,
+      },
+    ],
+    friday: [
+      {
+        title: 'ESLPro',
+        state: 'end',
+        time: 3600,
+      },
+    ],
+    saturday: [
+      {
+        title: 'Comedy',
+        state: 'begin',
+        time: 52200,
+      },
+      {
+        title: 'Comedy',
+        state: 'end',
+        time: 59400,
+      },
+      {
+        title: 'Nybyggerne',
+        state: 'begin',
+        time: 81600,
+      },
+    ],
+    sunday: [
+      {
+        title: 'Nybyggerne',
+        state: 'end',
+        time: 5400,
+      },
+      {
+        title: 'Dybvaaaaad',
+        state: 'begin',
+        time: 41400,
+      },
+      {
+        title: 'Dybvaaaaad',
+        state: 'end',
+        time: 43200,
+      },
+    ],
+  };
 });
